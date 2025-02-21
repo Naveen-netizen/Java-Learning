64d8b9df5e23cf3d584d89ebe26786666d05cf91
Naveen-netizen <pnaveenkumar082002@gmail.com>
2025-02-21 11:15:25 +0530

    Added a file

---
+++ feb19/Reverse2.java
@@ -0,0 +1,18 @@
+package feb19;
+
+public class Reverse2 {
+    public static void main(String[] args) {
+        System.out.println(reverse(1234));
+    }
+
+    static int reverse(int n) {
+        int digit = (int) (Math.log10(n) + 1);
+        int ans = 0;
+        for (int index = digit; index > 0; index--) {
+            int rem = n % 10;
+            ans += rem * (int) Math.pow(10, index - 1);
+            n = n / 10;
+        }
+        return ans;
+    }
+}
