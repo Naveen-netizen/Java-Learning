7504c65ad8989899cf1341feaaced82ad6c85665
Naveen-netizen <pnaveenkumar082002@gmail.com>
2025-03-04 19:09:45 +0530

    Added entire folder

---
+++ Subseq2.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+public class Subseq2 {
+    public static void main(String[] args) {
+        System.out.println(seq("", "abc"));
+    }
+
+    static ArrayList<String> seq(String p, String up) {
+
+        if (up.isEmpty()) {
+            ArrayList<String> ans = new ArrayList<>();
+            ans.add(p);
+            return ans;
+        }
+        
+        char ch = up.charAt(0);
+        ArrayList<String> right = seq(p + ch, up.substring(1));
+        ArrayList<String> left = seq(p, up.substring(1));
+        right.addAll(left);
+        return right;
+    }
+}
